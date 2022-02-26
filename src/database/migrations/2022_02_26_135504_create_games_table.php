a53f8eab8971ce11e66eec960f81586a86d9b402
marusan11 <tk.1126.hyt@gmail.com>
2022-02-26 15:27:31 +0900

    マイグレーション（BG管理)

---
+++ src/database/migrations/2022_02_26_135504_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('BGname');
+            $table->integer('good_leve');
+            $table->integer('number');
+            $table->string('time_required');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
