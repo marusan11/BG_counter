2b3b77882beddb63d3ff14f7b91b7213fb63497b
marusan11 <tk.1126.hyt@gmail.com>
2022-03-02 22:12:41 +0900

    gamesTable修正

--- src/database/migrations/2022_02_26_135504_create_games_table.php
+++ src/database/migrations/2022_02_26_135504_create_games_table.php
@@ -17,11 +17,12 @@ class CreateGamesTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->string('BGname');
+            $table->string('name');
             $table->integer('good_leve');
             $table->integer('number');
             $table->string('time_required');
             $table->integer('count');
+            $table->text('comment');
             $table->timestamps();
         });
     }
